b6bf5f0ddbb5ff04165a363dc8c75a6125af89db
Christopher P. Maher <chris@mahercode.io>
2018-07-11 01:46:14 -0700

    Pushing for work on tomorrow

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+%w(
+    slack 
+    vscode
+    autohotkeys
+)
+chocolatey_package 'package_name' do
+  action :install
+end
+
