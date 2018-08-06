eca675a4b3afaa0dfb402631d23330be9686a89c
Christopher Maher <Christopher.Maher@alaskaair.com>
2018-08-06 15:05:24 -0700

    Updated tests

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -5,14 +5,26 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
-end
-
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
-end
+%w(
+  chefdk
+  git
+  vscode
+  evernote
+  ConEmu
+  GoogleChrome
+  microsoft-teams
+  slack
+  zoom
+  habitat
+  vagrant
+  virtualbox 
+  docker-for-windows
+  SkypeForBusiness
+  classic-shell
+  autohotkey
+  7zip
+).each do |package|
+  describe chocolatey_package(package) do
+    it { should be_installed }
+  end 
+end 
